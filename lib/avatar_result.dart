372df7238b5b9f008c35ccede417a1114ed2c3aa
e_jescac <e_jescac@mercadona.es>
2020-05-14 18:23:47 +0200

    share feature

--- lib/avatar_result.dart
+++ lib/avatar_result.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:share/share.dart';
 import './constants.dart' as Constants;
 
 class AvatarResult extends StatelessWidget {
@@ -11,6 +12,7 @@ class AvatarResult extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String genderString = gender == Constants.Gender.female ? 'female' : 'male';
+    String avatarUrl = 'https://avatars.dicebear.com/v2/${genderString}/${nickname}.svg';
     return Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
       Expanded(
               child: Container(
@@ -22,7 +24,7 @@ class AvatarResult extends StatelessWidget {
             children: <Widget>[
               Text(nickname),
               SvgPicture.network(
-                'https://avatars.dicebear.com/v2/${genderString}/${nickname}.svg',
+                avatarUrl,
                 width: 100,
                 semanticsLabel: 'A shark?!',
                 placeholderBuilder: (BuildContext context) => Container(
@@ -33,6 +35,7 @@ class AvatarResult extends StatelessWidget {
                 onPressed: () {
                   Scaffold.of(context)
                       .showSnackBar(SnackBar(content: Text('Sharing')));
+                      Share.share('check out my pixel art avatar for ${nickname} (${genderString}): ${avatarUrl}', subject: 'Look your pixel art avatar!');
                 },
                 child: Text('Share'),
               )
